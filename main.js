3f0a445c4d3ff26e36ccb96029350d330ff0558e
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-06-18 14:55:02 +0300

    Completed the assignment

--- main.js
+++ main.js
@@ -4,6 +4,30 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const emptyHeart = document.querySelectorAll('.like-glyph');
+const errorModel = document.querySelectorAll('.hidden')[0];
+
+
+emptyHeart.forEach(heart => {
+  heart.addEventListener('click',onEmptyHeartClick)
+});
+
+function onEmptyHeartClick(event){
+   mimicServerCall().
+   then(response=>{
+    //alert(`This is the response I received ${response}`) -- test for a successful response.
+    event.target.textContent = FULL_HEART;         //adds the fullheart after a successful response.
+    event.target.classList.add('activated-heart');
+   })
+   .catch(error=>{
+    errorModel.classList.remove('hidden');
+    errorModel.querySelector('p').textContent = error;
+    setTimeout(() => {
+      errorModel.classList.add('hidden');
+    }, 3000);
+   })
+  //alert(' This is an empty heart that has been clicked.')  - test for correct grabbing of the html Element.
+}
 
 
 
